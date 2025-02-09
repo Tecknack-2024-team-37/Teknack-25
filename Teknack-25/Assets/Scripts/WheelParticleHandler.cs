e0bb72fc98d960686fbac4c0b4c0417587566a59
Pranit-DC <pranitchiman@gmail.com>
2025-02-09 17:37:07 +0530

    commit by pranit
    final smoke and skid effects with scripts

---
+++ Teknack-25/Assets/Scripts/WheelParticleHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelParticleHandler : MonoBehaviour
+{
+    //local variables
+    float particleEmissionRate = 0;
+
+    //components
+    TopDownCarController topDownCarController;
+    
+    ParticleSystem particleSystemSmoke;
+    ParticleSystem.EmissionModule particleSystemEmissionModule;
+
+    void Awake()
+    {
+        topDownCarController = GetComponentInParent<TopDownCarController>();
+
+        particleSystemSmoke = GetComponent<ParticleSystem>();
+
+        particleSystemEmissionModule = particleSystemSmoke.emission;
+
+        particleSystemEmissionModule.rateOverTime = 0;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //reduce the particle over time
+        particleEmissionRate = Mathf.Lerp(particleEmissionRate, 0, Time.deltaTime * 5);
+        particleSystemEmissionModule.rateOverTime = particleEmissionRate;
+
+        if (topDownCarController.IsTyreScreeching(out float lateralVelocity, out bool isBraking))
+        {
+            if(isBraking)
+                particleEmissionRate = 30;
+            else particleEmissionRate = Mathf.Abs(lateralVelocity) * 2;
+        }
+    }
+}
