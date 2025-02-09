def4d00c90588ddd733b482847503d5616ba4a64
codehb01 <harshalbhosale380@gmail.com>
2025-02-09 18:08:04 +0530

    commit by harshal

---
+++ Teknack-25/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+  public GameObject pauseUI;
+    // Start is called before the first frame update
+  public void OnRestartPress(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void OnGameResumePress(){
+        pauseUI.SetActive(false);
+  }
+
+  public void OnGameExitPress(){
+        Application.Quit();
+  }
+
+  public void OnEnterPausePress(){
+        pauseUI.SetActive(true);
+  }
+}
