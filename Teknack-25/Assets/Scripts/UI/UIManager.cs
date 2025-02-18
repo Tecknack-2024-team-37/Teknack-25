7ea8bb6829dd5e8734fa26c484db9eadc270fb12
Rakesh-hp <rakeshchaudhary70983@gmail.com>
2025-02-19 00:45:20 +0530

    commit by rakesh
    ..

--- Teknack-25/Assets/Scripts/UI/UIManager.cs
+++ Teknack-25/Assets/Scripts/UI/UIManager.cs
@@ -104,3 +104,109 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 }
+
+//gpt
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+// using UnityEngine.EventSystems; // Required for EventTrigger
+
+// public class UIManager : MonoBehaviour
+// {
+//     public GameObject pauseUI;
+//     public GameObject settingUI;
+//     private bool isPaused = false; 
+//     public GameObject Finish;
+//     private int lapsCompleted = 0;
+
+//     private CarUIInputHandler[] inputHandlers; // Reference to all input handlers
+//     private Button[] uiButtons; // Reference to UI buttons (Pause & Settings)
+//     private EventTrigger[] eventTriggers; // Reference to all EventTriggers
+
+//     private void Start()
+//     {
+//         inputHandlers = FindObjectsOfType<CarUIInputHandler>(); // Get all input handlers
+//         uiButtons = FindObjectsOfType<Button>(); // Get all UI buttons
+//         eventTriggers = FindObjectsOfType<EventTrigger>(); // Get all EventTriggers
+//     }
+
+//     private void ToggleInputHandlers(bool enable)
+//     {
+//         foreach (CarUIInputHandler handler in inputHandlers)
+//         {
+//             handler.enabled = enable;
+//         }
+
+//         foreach (EventTrigger trigger in eventTriggers)
+//         {
+//             trigger.enabled = enable;
+//         }
+//     }
+
+//     public void OnRestartPress()
+//     {
+//         Time.timeScale = 1f;
+//         lapsCompleted = 0;
+
+//         if (Finish != null)
+//             Finish.SetActive(false);
+
+//         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+//     }
+
+//     public void OnGameResumePress()
+//     {
+//         pauseUI.SetActive(false);
+//         isPaused = false;
+//         ToggleInputHandlers(true); 
+//     }
+
+//     public void OnGameExitPress()
+//     {
+//         Application.Quit();
+//     }
+
+//     public void OnEnterPausePress()
+//     {
+//         if (isPaused)
+//         {
+//             pauseUI.SetActive(false);
+//             settingUI.SetActive(false);
+//             isPaused = false;
+//             ToggleInputHandlers(true);
+//         }
+//         else
+//         {
+//             pauseUI.SetActive(true);
+//             settingUI.SetActive(false);
+//             isPaused = true;
+//             ToggleInputHandlers(false);
+//         }
+//     }
+
+//     public void OnEnterSettingsPress()
+//     {
+//         pauseUI.SetActive(false);
+//         settingUI.SetActive(true);
+//         ToggleInputHandlers(false);
+//     }
+
+//     public void OnExitSettingsPress()
+//     {
+//         settingUI.SetActive(false);
+//         pauseUI.SetActive(true);
+//         ToggleInputHandlers(false);
+//     }
+
+//     public void MainMenu()
+//     {
+//         SceneManager.LoadSceneAsync(0);
+//     }
+
+//     public void AllLevels()
+//     {
+//         SceneManager.LoadSceneAsync("Levels", LoadSceneMode.Single);
+//     }
+// }
