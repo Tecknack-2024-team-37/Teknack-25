cebdcc7d3249ca5671a6add08586b224f4736873
codehb01 <harshalbhosale380@gmail.com>
2025-02-28 21:13:13 +0530

    commit

--- Teknack-25/Assets/Scripts/Sound/SoundManager.cs
+++ Teknack-25/Assets/Scripts/Sound/SoundManager.cs
@@ -1,10 +1,51 @@
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class SoundManager : MonoBehaviour
+// {
+//     private AudioSource[] allButtonSounds;
+//     private bool isSoundOn = true;
+//     public Button soundToggleButton;
+//     public Sprite soundOnSprite;
+//     public Sprite soundOffSprite;
+//     private Image buttonImage;
+
+//     private void Start()
+//     {
+//         // Find all AudioSources in the scene (attached to buttons)
+//         allButtonSounds = FindObjectsOfType<AudioSource>();
+
+//         if (soundToggleButton != null)
+//         {
+//             buttonImage = soundToggleButton.GetComponent<Image>();
+//             soundToggleButton.onClick.AddListener(ToggleSound);
+//         }
+//     }
+
+//     public void ToggleSound()
+//     {
+//         isSoundOn = !isSoundOn;
+
+//         foreach (AudioSource audioSource in allButtonSounds)
+//         {
+//             audioSource.mute = !isSoundOn;
+//         }
+
+//         // Change button icon (if using sprites for sound on/off)
+//         if (buttonImage != null)
+//         {
+//             buttonImage.sprite = isSoundOn ? soundOnSprite : soundOffSprite;
+//         }
+//     }
+// }
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    private AudioSource[] allButtonSounds;
-    private bool isSoundOn = true;
+    private AudioSource[] allSounds; // To hold all AudioSources
+    private bool isSoundOn;
     public Button soundToggleButton;
     public Sprite soundOnSprite;
     public Sprite soundOffSprite;
@@ -12,29 +53,45 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
-        // Find all AudioSources in the scene (attached to buttons)
-        allButtonSounds = FindObjectsOfType<AudioSource>();
+        // Get all AudioSources in the scene
+        allSounds = FindObjectsOfType<AudioSource>();
 
+        // Get button image component
         if (soundToggleButton != null)
         {
             buttonImage = soundToggleButton.GetComponent<Image>();
             soundToggleButton.onClick.AddListener(ToggleSound);
         }
+
+        // Load previous sound setting (default is sound ON)
+        isSoundOn = PlayerPrefs.GetInt("SoundSetting", 1) == 1;
+
+        // Apply initial sound state
+        ApplySoundState();
     }
 
     public void ToggleSound()
     {
         isSoundOn = !isSoundOn;
+        PlayerPrefs.SetInt("SoundSetting", isSoundOn ? 1 : 0); // Save state
+        PlayerPrefs.Save();
 
-        foreach (AudioSource audioSource in allButtonSounds)
+        ApplySoundState();
+    }
+
+    private void ApplySoundState()
+    {
+        // Mute/unmute all sounds
+        foreach (AudioSource audioSource in allSounds)
         {
             audioSource.mute = !isSoundOn;
         }
 
-        // Change button icon (if using sprites for sound on/off)
+        // Change button icon
         if (buttonImage != null)
         {
             buttonImage.sprite = isSoundOn ? soundOnSprite : soundOffSprite;
         }
     }
 }
+
